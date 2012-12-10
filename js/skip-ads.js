b196537a90e8753501ae228d876910d555dbe233
Vignesh Nandha Kumar <viky.nandha@gmail.com>
2012-12-11 00:12:13 +0530

    Almost rewrote, for youtube's new interface

--- js/skip-ads.js
+++ js/skip-ads.js
@@ -1,25 +1,33 @@
-var is_html5 = $('#watch-player').find("video").length > 0;
+( function() {
+    var $embed, $skip_btn, is_html5;
+    $embed = $( 'embed[src*="//s.ytimg.com/"]' );
+    is_html5 = $('#watch-player').find("video").length > 0;
 
-$('#watch-headline-user-info').append(
-  '<button id="skip-ad" class="yt-uix-button yt-uix-button-default">Skip ad</button>');
-$('#skip-ad').on('click', function() {
-  if($(this).attr('disabled')) return false;
-  skip_ad();
-  $(this).attr('disabled', 'disabled');
-  return false;
-});
+    $( '<button id="chrome-extension-skip-ad-btn">Skip ad</button>' )
+        .appendTo( 'body' )
+        .on( 'click', function() {
+            if( $( this ).attr( 'disabled' ) ) {
+                return;
+            }
+            skip_ad();
+            $( this ).attr( 'disabled', 'disabled' );
+        });
 
-function skip_ad() {
-  if (is_html5) {
-    $('div.video-ads').hide();
-    return true;
-  }
+    function skip_ad() {
+        if (is_html5) {
+            $('div.video-ads').hide();
+            return true;
+        }
 
-  var embed_elem = $('#watch-player').children('embed').clone();
-  embed_elem.attr('src' , 'http://www.youtube.com/v/' +
-                  location.href.split("v=")[1].split("&")[0] +
-                  '&version=3&autoplay=1&fs=1');
-  $('#watch-player').children('embed').remove();
-  $('#watch-player').prepend(embed_elem);
-  return true;
-}
+        var $embed_new;
+        $embed_new = $embed.clone();
+        $embed_new.attr( 'src',
+                         'http://www.youtube.com/v/' +
+                         location.href.split( 'v=' )[1].split( '&' )[0] +
+                         '&version=3&autoplay=1&fs=1' );
+        $embed.replaceWith( $embed_new );
+        return true;
+    }
+
+    skip_ad();
+} ) ();
